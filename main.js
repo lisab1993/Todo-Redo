b29fc18f6e50e41762a6b1395954213a33933204
lisab1993 <brown.lisa999@gmail.com>
2021-04-28 19:36:36 -0700

    working on todolist v2 in class.

---
+++ main.js
@@ -0,0 +1,50 @@
+const Todos = require('./todo').Todos
+const yo = require('yo-yo')
+
+const todos = [
+  { text: 'take out the trash', completed: true },
+  { text: 'do the dishes', completed: false },
+]
+
+const todoDiv = document.getElementById('todos')
+const todoInput = document.getElementById('todo-input')
+const addTodo = document.getElementById('add-todo')
+const showAll = document.getElementById('all')
+const showCompleted = document.getElementById('completed')
+const showIncomplete = document.getElementById('incomplete')
+
+showAll.onclick = () => {
+  filter = filterAll
+  update()
+}
+
+showCompleted.onclick = () => {
+  filter = filterCompleted
+  update()
+}
+
+showIncomplete.onclick = () => {
+  filter = filterIncomplete
+  update()
+}
+
+const filterAll = todo => true
+const filterCompleted = todo => todo.completed
+const filterIncomplete = todo => !todo.completed
+
+let filter = filterAll
+
+addTodo.onsubmit = event => {
+  event.preventDefault()
+
+  const newTodoText = todoInput.value
+  todos.push({ text: newTodoText, completed: false })
+  update()
+}
+
+function update () {
+  yo.update(el, Todos(todos, filter, update))
+}
+
+const el = Todos(todos, filter, update)
+todoDiv.appendChild(el)
